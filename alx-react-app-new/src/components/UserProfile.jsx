e5e6fe52ec93ee73fd599343d2f28ec7f691a8bd
Yonatan Wolde <yonatanwolde251@gmail.com>
2025-09-07 12:04:54 +0300

    Feature-Styling components_01

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,8 +2,8 @@ const UserProfile = (props) => {
     return (
         <div style={{
             border: '1px solid gray',
-            padding: '12px',
-            margin: '12px',
+            padding: '10px',
+            margin: '10px',
         }}>
             <h2 style={{
                 color: 'blue',
