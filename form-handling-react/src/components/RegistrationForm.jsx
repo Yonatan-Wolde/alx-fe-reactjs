2130a3b99edf5e78da480f5d230606c66575e37e
Yonatan Wolde <yonatanwolde251@gmail.com>
2025-10-12 13:48:39 +0300

    initial commit 01

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -7,6 +7,8 @@ const RegistrationForm = () => {
     password: ''
   });
 
+  const { username, email, password } = formData;
+
   const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
@@ -19,9 +21,9 @@ const RegistrationForm = () => {
 
   const validate = () => {
     let tempErrors = {};
-    if (!formData.username) tempErrors.username = "Username is required";
-    if (!formData.email) tempErrors.email = "Email is required";
-    if (!formData.password) tempErrors.password = "Password is required";
+    if (!username) tempErrors.username = "Username is required";
+    if (!email) tempErrors.email = "Email is required";
+    if (!password) tempErrors.password = "Password is required";
     setErrors(tempErrors);
     return Object.keys(tempErrors).length === 0;
   };
@@ -29,30 +31,44 @@ const RegistrationForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validate()) {
-      console.log("Submitting Data:", formData);
-      // Simulate API call
-      alert("Form submitted successfully!");
+      console.log("Submitting:", formData);
+      alert("Form submitted!");
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label><br/>
-        <input type="text" name="username" value={formData.username} onChange={handleChange} />
-        {errors.username && <p style={{color: 'red'}}>{errors.username}</p>}
+        <label>Username:</label><br />
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+        />
+        {errors.username && <p style={{ color: 'red' }}>{errors.username}</p>}
       </div>
 
       <div>
-        <label>Email:</label><br/>
-        <input type="email" name="email" value={formData.email} onChange={handleChange} />
-        {errors.email && <p style={{color: 'red'}}>{errors.email}</p>}
+        <label>Email:</label><br />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
+        {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
       </div>
 
       <div>
-        <label>Password:</label><br/>
-        <input type="password" name="password" value={formData.password} onChange={handleChange} />
-        {errors.password && <p style={{color: 'red'}}>{errors.password}</p>}
+        <label>Password:</label><br />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
+        {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
       </div>
 
       <button type="submit">Register</button>
