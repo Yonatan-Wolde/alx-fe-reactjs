a80a460f7085cb84c919335cb244b91c4c451747
Yonatan Wolde <yonatanwolde251@gmail.com>
2025-10-12 15:22:47 +0300

    task 03 correction 01

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from 'react'; // <--- ADD THIS LINE
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import TodoList from '../components/TodoList';
+import TodoList from './TodoList';
 
 // ... rest of the test file
 
